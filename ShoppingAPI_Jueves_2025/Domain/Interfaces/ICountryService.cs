bf58ce096035604ec348d2bb072d0ea6bbfd0434
daniel13git <danielcano288538@correo.itm.edu.co>
2025-05-12 23:08:16 -0500

    Creacion de ICountryService

---
+++ ShoppingAPI_Jueves_2025/Domain/Interfaces/ICountryService.cs
@@ -0,0 +1,12 @@
+﻿using ShoppingAPI_Jueves_2025.DAL.Entities;
+namespace WebAPI.Domain.Interfaces
+{
+    public interface ICountryService
+    {
+        Task<IEnumerable<Country>> GetCountriesAsync(); // firma del metodo
+        Task<Country> CreateCountryAsync(Country country);
+        Task<Country> GetCountryByIdAsync(Guid id);
+        Task<Country> EditCountryAsync(Country country);
+        Task<Country> DeleteCountryAsync(Guid id);
+    }
+}
