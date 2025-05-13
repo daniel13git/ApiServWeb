e2f39c6fc178a75f0040c8410c1c512adf0faa36
daniel13git <danielcano288538@correo.itm.edu.co>
2025-05-13 00:01:03 -0500

    primera implementacion de CountryService

--- ShoppingAPI_Jueves_2025/Domain/Interfaces/ICountryService.cs
+++ ShoppingAPI_Jueves_2025/Domain/Interfaces/ICountryService.cs
@@ -3,9 +3,10 @@ namespace WebAPI.Domain.Interfaces
 {
     public interface ICountryService
     {
-        Task<IEnumerable<Country>> GetCountriesAsync(); // firma del metodo
-        Task<Country> CreateCountryAsync(Country country);
+        // firma de los metodos
+        Task<IEnumerable<Country>> GetCountriesAsync();
         Task<Country> GetCountryByIdAsync(Guid id);
+        Task<Country> CreateCountryAsync(Country country);
         Task<Country> EditCountryAsync(Country country);
         Task<Country> DeleteCountryAsync(Guid id);
     }
--- ShoppingAPI_Jueves_2025/Domain/Services/CountryService.cs
+++ ShoppingAPI_Jueves_2025/Domain/Services/CountryService.cs
@@ -1,6 +1,42 @@
-﻿namespace WebAPI.Domain.Services
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+using ShoppingAPI_Jueves_2025.DAL;
+using ShoppingAPI_Jueves_2025.DAL.Entities;
+using WebAPI.Domain.Interfaces;
+namespace WebAPI.Domain.Services
+
 {
-    public class CountryService
+    public class CountryService : ICountryService
     {
+        private readonly DataBaseContext _context;
+        public CountryService(DataBaseContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Country>> GetCountriesAsync()
+        {
+            return await _context.Countries.ToListAsync();
+        }
+
+        public Task<Country> GetCountryByIdAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Country> CreateCountryAsync(Country country)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Country> EditCountryAsync(Country country)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Country> DeleteCountryAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
