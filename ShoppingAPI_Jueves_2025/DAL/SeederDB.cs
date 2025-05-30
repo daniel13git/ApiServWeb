8cbda85fc26422c98ada6225ebccda2f5e5b6cf2
daniel13git <danielcano288538@correo.itm.edu.co>
2025-05-29 20:07:34 -0500

    Creacion del SeederDB

---
+++ ShoppingAPI_Jueves_2025/DAL/SeederDB.cs
@@ -0,0 +1,78 @@
+﻿using ShoppingAPI_Jueves_2025.DAL;
+using ShoppingAPI_Jueves_2025.DAL.Entities;
+using WebAPI.DAL.Entities;
+
+namespace WebAPI.DAL
+{
+    public class SeederDB
+    {
+        // Se hace la inyeccion de dependencias
+        private readonly DataBaseContext _context;
+
+        public SeederDB(DataBaseContext context)
+        {
+            _context = context;
+        }
+
+        //Se crea un metodo llamado "SeederAsync", es como una especie de metodo Main
+        // Tendra la responsabilidad de prepoblar las diferentes tablas de la BD
+
+        public async Task SeederAsync()
+        {
+            //primero agrega un metodo propio de Entity Framework que hace las veces del comando "update-database"
+            //en otras palabras, un metodo que me creara la BD inmediatamente ponga la ejecucion la API
+            await _context.Database.EnsureCreatedAsync ();
+
+            //ahora se crean metodos para prepoblar la BD
+            await PopulateCountriesAsync();
+            await _context.SaveChangesAsync(); // guarda los datos en la BD
+
+        }
+
+        #region Private Methos
+        private async Task PopulateCountriesAsync()
+        {
+            //el metodo Any() indica que en la tabla haya al menos 1 registro
+            //el Any negado (!) indica que en la tabla no hay absolutamente nada
+
+            if (!_context.Countries.Any()) 
+            {
+                // asi se crea un pais con sus respectivos estados
+                _context.Countries.Add(new Country
+                {
+                    CreatedDate = DateTime.Now,
+                    Name = "Colombia",
+                    States = new List<State>()
+                    {
+                        new State 
+                        {
+                            CreatedDate = DateTime.Now, 
+                            Name = "Antioquia"
+                        },
+
+                        new State
+                        {
+                            CreatedDate = DateTime.Now, 
+                            Name = "Cundinamarca"
+                        }
+                    }
+                });
+
+                _context.Countries.Add(new Country
+                {
+                    CreatedDate = DateTime.Now,
+                    Name = "Argentina",
+                    States = new List<State>()
+                    {
+                        new State
+                        {
+                            CreatedDate = DateTime.Now, 
+                            Name = "Buenos Aires"
+                        }
+                    }
+                });
+            }
+        }
+        #endregion
+    }
+}
--- ShoppingAPI_Jueves_2025/Program.cs
+++ ShoppingAPI_Jueves_2025/Program.cs
@@ -1,6 +1,8 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using ShoppingAPI_Jueves_2025.DAL;
 using ShoppingAPI_Jueves_2025.DAL.Entities;
+using WebAPI.DAL;
 using WebAPI.Domain.Interfaces;
 using WebAPI.Domain.Services;
 
@@ -19,8 +21,21 @@ builder.Services.AddScoped<ICountryService, CountryService>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddTransient<SeederDB>();
 
 var app = builder.Build();
+
+SeederData();
+
+void SeederData()
+{
+    IServiceScopeFactory? scopedFactory = app.Services.GetService<IServiceScopeFactory>();
+    using (IServiceScope? scope = scopedFactory.CreateScope())
+    {
+        SeederDB? service = scope.ServiceProvider.GetService<SeederDB>();
+        service.SeederAsync().Wait();
+    }
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
